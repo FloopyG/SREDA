90f8af18b658c5e1e38bebbdb32bc93ab34eadf6
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-09-22 16:07:38 +0300

    ForPadla

--- preloader.js
+++ preloader.js
@@ -1,4 +1,6 @@
 window.onload = function () {
 	let preloader = document.getElementById('preloader');
+	let body = document.getElementById('body');
 	preloader.style.display = 'none';
+	body.style.overflow = 'auto';
 };
